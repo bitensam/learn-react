87274fe44625fbf88017a1c47d457a6a44bc6486
Jakub Wojcik <j.wojcik1994@gmail.com>
2021-07-11 18:22:28 +0200

    Working filtering and search cards

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,33 @@
+// selectors
+
+/* wartość właściwości searchString, zapisanej bezpośrednio w stanie aplikacji */
+export const getSearchString = ({searchString}) => searchString;
+
+/* wyszukać karty pasujące do frazy searchString, a następnie je policzyć. */
+export const countAllCards = ({cards}) => cards.length;
+
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+
+// action name creator
+
+const reducerName = 'searchString';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// actions types
+
+export const CHANGE = createActionName('CHANGE');
+
+// action creators
+
+export const createAction_changeSearchString = payload => ({payload, type: CHANGE });
+
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case CHANGE:
+      return action.payload;
+    default:
+      return statePart;
+  }
+}
+
